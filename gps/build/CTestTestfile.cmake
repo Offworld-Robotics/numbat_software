982d4728770408fd3e49d7f7c804b8ba73b26c1a
hjed <harry@dayfamilyweb.com>
2014-05-14 21:02:59 +1000

    For issue #16 Ported agvc_gps to catkin_make and made it portable Note: need to port agvc_sensors to make it work

--- gps/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/owr/git/owr_software/gps/src
-# Build directory: /home/ros/owr/git/owr_software/gps/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
