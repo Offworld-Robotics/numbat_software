d826ffd6715517cf4dd554a0fc03ec096987dd6d
hjed <harry@dayfamilyweb.com>
2014-05-30 22:12:16 +1000

    For #21 - Started working on reading gps output and writting kml files. Currently experiencing some catkin errors that are preventing compilation.

---
+++ gps/src/owr_gps_logging/include/owr_gps_logging/gpsLogger.h
@@ -0,0 +1,51 @@
+/*
+ *This service logs the gps output as gpx and kml files
+ *Created by Harry J.E Day for BlueSat UNSW
+ *Date 30/05/2014
+ */
+
+#include <ros/ros.h>
+
+#include <sensor_msgs/navSatFix.h>
+#define FLOAT_BYTES 4
+#define LOG_FILE "~/logs/log.kml"
+#define KML_HEADER "<?xml version="1.0" encoding="UTF-8"?>" \ 
+"<kml xmlns=\"http://www.opengis.net/kml/2.2\">" \ 
+"  <Document>" \ 
+"    <name>Paths</name>" \ 
+"    <description>ROS Log</description>" \ 
+"    <Style id=\"yellowLineGreenPoly\">" \ 
+"      <LineStyle>" \ 
+"        <color>7f00ffff</color>" \ 
+"        <width>4</width>" \ 
+"      </LineStyle>" \ 
+"      <PolyStyle>" \ 
+"        <color>7f00ff00</color>" \ 
+"      </PolyStyle>" \ 
+"    </Style>" \ 
+"    <Placemark>" \ 
+"      <name>Absolute Extruded</name>" \ 
+"      <description>Transparent green wall with yellow outlines</description>" \ 
+"      <styleUrl>#yellowLineGreenPoly</styleUrl>" \ 
+"      <LineString>" \ 
+"        <extrude>1</extrude>" \ 
+"        <tessellate>1</tessellate>" \ 
+"        <altitudeMode>absolute</altitudeMode>" \ 
+"        <coordinates>"
+
+
+#define KML_FOOTER \
+"        </coordinates>" \ 
+"      </LineString>" \ 
+"    </Placemark>" \ 
+"  </Document>" \ 
+"</kml>"
+
+class GPSLogger {
+
+    public:
+        GPSLogger();
+        void reciveMsg(const sensor_msgs::NavSatFix::ConstPtr& msg);
+    private:
+        char * coOrdList = NULL;
+};
