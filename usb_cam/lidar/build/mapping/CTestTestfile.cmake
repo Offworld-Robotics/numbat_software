621e6b97a5bc11d38e9c942ed5116b4b699b8ff6
ros <harry@dayfamilyweb.com>
2014-06-14 13:49:55 +1000

    Got usb_cam drivers so we can edit them. For issue #20

--- usb_cam/lidar/build/mapping/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/owr/owr_software/lidar/src/mapping
-# Build directory: /home/ros/owr/owr_software/lidar/build/mapping
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
