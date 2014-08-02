916372f8de5c67bb063008a3aac7772655c8ffdb
ros <harry@dayfamilyweb.com>
2014-08-02 12:22:45 +1000

    Cleaned up the git repo a bit

--- lidar/build/catkin_generated/order_packages.py
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/ros/owr/owr_software/lidar/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
