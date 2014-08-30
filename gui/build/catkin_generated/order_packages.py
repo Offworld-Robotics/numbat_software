7aa9e9f68fdb0e2716c9f8baf680ba1eb6e9aacd
YiweiHan <yiweih@live.com>
2014-08-30 11:23:33 +1000

    For #28, some experimental thing using glDrawPixels.

--- gui/build/catkin_generated/order_packages.py
+++ gui/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/yiweih/owr/owr_software/gui/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
+underlay_workspaces = "/home/yiweih/owr/owr_software/gui/devel;/opt/ros/indigo".split(';') if "/home/yiweih/owr/owr_software/gui/devel;/opt/ros/indigo" != "" else []
--- gui/build/catkin_generated/setup_cached.sh
+++ gui/build/catkin_generated/setup_cached.sh
@@ -5,16 +5,8 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
-export CATKIN_TEST_RESULTS_DIR="/home/yiweih/owr/owr_software/gui/build/test_results"
-export ROS_TEST_RESULTS_DIR="/home/yiweih/owr/owr_software/gui/build/test_results"
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/yiweih/owr/owr_software/gui/devel:$CMAKE_PREFIX_PATH"
-export CPATH="/home/yiweih/owr/owr_software/gui/devel/include:$CPATH"
-export LD_LIBRARY_PATH="/home/yiweih/owr/owr_software/gui/devel/lib:/home/yiweih/owr/owr_software/gui/devel/lib/i386-linux-gnu:/opt/ros/indigo/lib/i386-linux-gnu:/opt/ros/indigo/lib"
-export PATH="/home/yiweih/owr/owr_software/gui/devel/bin:$PATH"
-export PKG_CONFIG_PATH="/home/yiweih/owr/owr_software/gui/devel/lib/pkgconfig:/home/yiweih/owr/owr_software/gui/devel/lib/i386-linux-gnu/pkgconfig:/opt/ros/indigo/lib/i386-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export LD_LIBRARY_PATH="/home/yiweih/owr/owr_software/gui/devel/lib/i386-linux-gnu:/opt/ros/indigo/lib/i386-linux-gnu:/home/yiweih/owr/owr_software/gui/devel/lib:/opt/ros/indigo/lib"
+export PKG_CONFIG_PATH="/home/yiweih/owr/owr_software/gui/devel/lib/i386-linux-gnu/pkgconfig:/opt/ros/indigo/lib/i386-linux-gnu/pkgconfig:/home/yiweih/owr/owr_software/gui/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/yiweih/owr/owr_software/gui/build"
-export PYTHONPATH="/home/yiweih/owr/owr_software/gui/devel/lib/python2.7/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/yiweih/owr/owr_software/gui/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/yiweih/owr/owr_software/gui/src:$ROS_PACKAGE_PATH"
