ea415835a74f8689871b5da7b7a1788cc5767e25
YiweiHan <yiweih@live.com>
2014-09-06 13:17:54 +1000

    For #28, drew some more things like battery and signal.

--- gui/build/owr_gui/catkin_generated/installspace/owr_guiConfig.cmake
+++ gui/build/owr_gui/catkin_generated/installspace/owr_guiConfig.cmake
@@ -122,7 +122,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/yiweih/owr/owr_software/gui/install/lib;/home/yiweih/owr/owr_software/gui/devel/lib;/opt/ros/indigo/lib)
+    foreach(path /home/yiweih/owr/owr_software/gui/install/lib;/opt/ros/indigo/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
