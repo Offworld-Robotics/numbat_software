184f441d0afca97c4a0b9f004ca1a45f477d7b47
ros <harry@dayfamilyweb.com>
2015-01-10 15:36:15 +1100

    Debuging output

--- rover/src/owr_camera_control/script/activiate_feeds.py
+++ rover/src/owr_camera_control/script/activiate_feeds.py
@@ -6,8 +6,9 @@ from owr_camera_control.msg import stream
 
 #called when the message is recived
 def callback(data):
-
+    print data
     if data.on:
+        print "on"
         os.environ["GSCAM_CONFIG"] = "v4l2src device=/dev/video"+str(data.stream)+" ! video/x-raw-rgb,framerate=30/1 ! ffmpegcolorspace"
         call([ "rosrun","gscam", "gscam","__name=" + str(data.stream) + "_camera","gscam_publisher:=\/cam" + str(data.stream)])
     else:
