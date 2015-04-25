c816265c03416e84866dc0f4107eb74e4e4d128d
ros <harry@dayfamilyweb.com>
2015-04-25 11:39:18 +1000

    Lots of cleanup of unsused folders

--- husky_control/catkin_ws/build/Drive_Husky/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/owr/owr_software/husky_control/catkin_ws/src/Drive_Husky
-# Build directory: /home/ros/owr/owr_software/husky_control/catkin_ws/build/Drive_Husky
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- husky_control/catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/owr/owr_software/husky_control/catkin_ws/src/beginner_tutorials
-# Build directory: /home/ros/owr/owr_software/husky_control/catkin_ws/build/beginner_tutorials
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
