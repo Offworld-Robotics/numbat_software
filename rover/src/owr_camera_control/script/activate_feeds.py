3d777a345f75afdc757a7cd3f4ebccb1cd033b5e
Simon Ireland <spir109@cse.unsw.edu.au>
2015-05-01 20:15:38 -0700

    Added some defines for individual motor multipliers (between 0 and 1 pls)  so that mech guys can begin testing on the turning.  #71

---
+++ rover/src/owr_camera_control/script/activate_feeds.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import rospy
+import os
+import subprocess 
+from owr_messages.msg import stream
+
+#called when the message is recived
+def callback(data):
+    print data
+    if data.on:
+        print "on"
+        os.environ["GSCAM_CONFIG"] = "v4l2src device=/dev/video"+str(data.stream)+" ! video/x-raw-rgb,framerate=30/1,width=640,height=480 ! ffmpegcolorspace"
+        subprocess.Popen([ "rosrun","gscam", "gscam","__name=" + str(data.stream) + "_camera","gscam_publisher:=\/cam" + str(data.stream)])
+    else:
+        subprocess.call(["rosnode","kill","/gscam_publisher"]);
+        return
+        
+def activateFeeds():
+    rospy.init_node('activateFeeds')
+    rospy.Subscriber("owr/control/activateFeeds", stream, callback)
+    print "running"
+    #loops while ros is running
+    rospy.spin()
+    
+    
+if __name__ == '__main__':
+    activateFeeds()
