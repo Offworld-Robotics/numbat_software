557604afa23e3f73157c7e7cfe9a68b4f38be7b1
Simon Ireland <spir109@cse.unsw.edu.au>
2015-05-01 22:07:56 -0700

    IMplements a new senstitivity formula output = ax^3 + (1-a)x for a more rounded input->output that can be altered from linear to x^3 curves #66

--- rover/src/owr_drive_controls/src/arduinoJoy.cpp
+++ rover/src/owr_drive_controls/src/arduinoJoy.cpp
@@ -70,9 +70,9 @@ void ArduinoConverter::switchFeed(int * storedState, int joyState, int feedNum) 
 void ArduinoConverter::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
     #define MAX_IN 1.0
     #define DIFF 0.25
-    #define POWER_LIMIT 0.2
-    float power = joy->axes[DRIVE_AXES_UD] * POWER_LIMIT;
-    float lr = (-joy->axes[STICK_L_LR]) * POWER_LIMIT;
+    #define SENSITIVITY 1
+    float power = joy->axes[DRIVE_AXES_UD];
+    float lr = (-joy->axes[STICK_L_LR]);
     
     //float leftDrive  = 1.0f;
     //float rightDrive = 1.0f;
@@ -80,8 +80,8 @@ void ArduinoConverter::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
     float lDrive  =   (power + lr)/2;
     float rDrive =   -(power - lr)/2;
     
-    leftDrive = ((lDrive / POWER_LIMIT * MAX_IN) * 500) + 1500.0  ;
-    rightDrive = ((rDrive / POWER_LIMIT * MAX_IN) * 500) + 1500.0  ;
+    leftDrive = ((SENSITIVITY * pow(lDrive, 3) + (1 - SENSISTIVITY) * ldrive) * 500) + 1500.0;
+    rightDrive = ((SENSITIVITY * pow(rDrive, 3) + (1 - SENSITIVITY) * ldrive) * 500) + 1500.0;
     
     /*if (joy->axes[STICK_LT]) {
         lfDrive = leftDrive;
