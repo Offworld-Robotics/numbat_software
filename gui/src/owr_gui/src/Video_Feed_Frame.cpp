5c973b6cd3eca66e860ecbd6d3457e218794119e
Sean Thompson <sean.thompson@student.unsw.edu.au>
2015-05-28 15:52:57 +1000

    Implemented displayVideo method of the Video_Feed_Frame
    also commented in copy  of method to allow a ROS process to update the
    texture that contains the actual frame of the video stream.
    Signature and use of this may ned to be redesigned depending on ROS
    process and this.

--- gui/src/owr_gui/src/Video_Feed_Frame.cpp
+++ gui/src/owr_gui/src/Video_Feed_Frame.cpp
@@ -13,22 +13,47 @@ Video_Feed_Frame::Video_Feed_Frame(int _centreX, int _centreY, int width, int he
    centreY = _centreY;
    halfWidth = width/2;
    halfHeight = height/2;
-
-}
-void Video_Feed_Frame::displayFrame(){
-   glPushMatrix();
-   glEnable(GL_TEXTURE_2D);
-   glColor3f(1, 1, 1);
-   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	
+   // Setup the texture to store frames of the video feed into
+   glGenTextures(1, &videoTexture);
    glBindTexture(GL_TEXTURE_2D, videoTexture);
-   glBegin(GL_QUADS);
-          glTexCoord2f(0, 1); glVertex2i(0, -halfWidth); // Bottom Left
-          glTexCoord2f(1, 1); glVertex2i(-halfHeight, halfWidth); // Bottom Right
-          glTexCoord2f(1, 0); glVertex2i(halfHeight, 0); // Top Right
-          glTexCoord2f(0, 0); glVertex2i(0, 0); // Top Left
-   glEnd();
-   glDisable(GL_TEXTURE_2D);
-   glPopMatrix();
-}	
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+}
+
+/* CHECK IF REQUIRES MODIFICATION FOR COMPATIBILITY
+// called by ROS process to set the contents of the texture to be the next frame of the video stream
+video_Feed_Frame::setNewStreamFrame(unsigned char *frame, int width, int height) {
+	if (frame != NULL) {
+		glBindTexture(GL_TEXTURE_2D, videoTexture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, frame);
+	}
+	
+	//ROS_INFO("Updated video");
+}
+*/
+
+// actually draw the Video_Feed_Frame to screen
+void Video_Feed_Frame::displayFrame() {
+	glPushMatrix();
+	glEnable(GL_TEXTURE_2D);
+	glColor3f(1, 1, 1);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	glBindTexture(GL_TEXTURE_2D, videoTexture);
+
+	// data from frame array is flipped, texcoords were changed to compensate
+	glBegin(GL_QUADS);
+		glTexCoord2f(0, 1); glVertex2i(centreX-halfWidth, centreY-halfHeight); // Bottom Left
+		glTexCoord2f(1, 1); glVertex2i(centreX+halfWidth, centreY-halfHeight); // Bottom Right
+		glTexCoord2f(1, 0); glVertex2i(centreX+halfWidth, centreY+halfHeight); // Top Right
+		glTexCoord2f(0, 0); glVertex2i(centreX-halfWidth, centreY+halfHeight); // Top Left
+	glEnd();
+        
+        // must disable texturing when done or graphical glitches occur
+	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+}
 
 #endif
