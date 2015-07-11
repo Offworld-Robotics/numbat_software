41aa4a44910a287bec16f30de619ba81c1fba1ac
Simon Ireland <spir109@cse.unsw.edu.au>
2015-07-11 15:41:54 +1000

    OWRS-72 Autonomous pathing of the rover, small commit to add file

---
+++ rover/src/owr_drive_controls/include/BoardControl.h
@@ -0,0 +1,50 @@
+/*
+ * Converts joystick comands to velocity, etc
+ * Author: Harry J.E Day for BlueSat OWR
+ * Start: 8/07/15
+ */
+#ifndef BOARD_CONTROL_H
+#define BOARD_CONTROL_H
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <geometry_msgs/Twist.h>
+#include <termios.h>
+#include <stdio.h>
+#include "buttons.h"
+
+using namespace std; 
+
+
+//serial IO
+#define TTY "/dev/ttyACM1"
+
+class BoardControl {
+    public:
+        BoardControl();
+        void run();
+        
+    private:
+        void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+        void armCallback(const sensor_msgs::Joy::ConstPtr& joy);
+        void switchFeed(int * storedState, int joyState, int feedNum);
+        //void sendMessage(float lf, float lm, float lb, float rf, float rm, float rb);
+        void sendTwist(void);
+        
+        ros::NodeHandle nh;
+        ros::Publisher  velPublisher;
+        ros::Subscriber joySubscriber;
+        ros::Subscriber armSubscriber;
+        float leftDrive, rightDrive;
+        //arm top, bottom
+        int armTop, armBottom;
+        float armRotate;
+        geometry_msgs::Twist vel;
+        
+        //to keep track of button states. It is possible press could change it
+        int cam0Button, cam1Button, cam2Button, cam3Button;
+        
+        //serial i0
+        FILE * fd;
+};
+
+#endif
