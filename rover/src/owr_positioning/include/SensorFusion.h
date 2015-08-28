13593c93351f823cf0b7a7a3f62240b9b22c10c6
Yilser Kabaran <yilserkabaran@gmail.com>
2015-08-28 20:38:21 +1000

    heading works

--- rover/src/owr_positioning/include/SensorFusion.h
+++ rover/src/owr_positioning/include/SensorFusion.h
@@ -1,32 +1,44 @@
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Vector3.h>
-
-class DataFusion{
+#include <ros/ros.h>
+#include "owr_messages/heading.h"
+class SensorFusion{
     public:
-        DataFusion();
+        SensorFusion();
         void spin();
     protected:
-        void receiveMag();
-        void receiveAccel();
-        void receiveGyro();
+        void receiveMag(const geometry_msgs::Vector3::ConstPtr&);
+        void receiveAccel(const geometry_msgs::Vector3::ConstPtr&);
+        void receiveGyro(const geometry_msgs::Vector3::ConstPtr&);
+        void fuseData();
         ros::Subscriber subMag;
         ros::Subscriber subAccel;
         ros::Subscriber subGyro;
         ros::Publisher pub;
 
-        geometry::Vector3 mag;
-        geometry::Vector3 acc;
-        geometry::Vector3 gyro;
+        geometry_msgs::Vector3 mag;
+        geometry_msgs::Vector3 acc;
+        geometry_msgs::Vector3 gyro;
 
+        geometry_msgs::Vector3 dcm[3];
+
+        double roll;
+        double pitch;
+        double yaw;
+        double heading;
+
+        unsigned char allThree; 
     private:
         ros::NodeHandle node;
+        
+        double vector_mod(geometry_msgs::Vector3);
+        void vector_normalize(geometry_msgs::Vector3);
+        void vector_cross(geometry_msgs::Vector3, geometry_msgs::Vector3, geometry_msgs::Vector3);
+        double vector_dot(geometry_msgs::Vector3, geometry_msgs::Vector3); 
+        void vector_scale(double ,geometry_msgs::Vector3, geometry_msgs::Vector3);
+        void vector_add(geometry_msgs::Vector3, geometry_msgs::Vector3, geometry_msgs::Vector3); 
+        void DCM_othonormalize(geometry_msgs::Vector3 [3]);
+        void DCM_rotate (geometry_msgs::Vector3 [3],geometry_msgs::Vector3);
+};
 
-        double vector_mod(geometry::Vector3);
-        void vector_normalize(geometry::Vector3);
-        void vector_cross(geometry::Vector3, geometry::Vector3, geometry::Vector3);
-        double vector_dot(geometry::Vector3, geometry::Vector3) 
-        void vector_scale(double ,geometry::Vector3, geometry::Vector3);
-        void vector_add(geometry::Vector3, geometry::Vector3, geometry::Vector3); 
-        void DCM_othonormalize(geometry::Vector3 [3]);
-        void DCM_rotate (geometry::Vector3 [3],geometry::Vector3);
-}
+double radToDeg (double);
--- rover/src/owr_positioning/src/SensorFusion.cpp
+++ rover/src/owr_positioning/src/SensorFusion.cpp
@@ -1,14 +1,16 @@
 #include "SensorFusion.h"
 #include <cmath>
 #define _USE_MATH_DEFINES
+#include <ros/ros.h>
+#include <geometry_msgs/Vector3.h>
 #define ACC_WEIGHT 0.05 
 #define MAG_WEIGHT 0.0 
 #define SYDNEY 12.33
 #define POLAND 4.48
-
+#define GYRO_HZ 5
 int
-main(int argc, char  argv) {
-    ros::init(argc, argv, "owr_sensor_fusion__node");
+main(int argc, char ** argv) {
+    ros::init(argc, argv, "owr_sensor_fusion_node");
     
     SensorFusion p;
     p.spin();
@@ -19,98 +21,116 @@ main(int argc, char  argv) {
 
 SensorFusion::SensorFusion() {
     //set up subs and pub
+    subMag = node.subscribe("/mag", 5, &SensorFusion::receiveMag, this);
+    
+    subAccel = node.subscribe("/acc", 5, &SensorFusion::receiveAccel, this);
+    subGyro = node.subscribe("/gyro", 5, &SensorFusion::receiveGyro, this);
+    pub = node.advertise<owr_messages::heading>("/owr/heading",10);
+    
 }
 
 void 
-SensorFusion::receiveMag() {
-
+SensorFusion::receiveMag(const geometry_msgs::Vector3::ConstPtr& _mag) {
+    allThree |= 0x1;
+    mag.x = _mag->x;
+    mag.y = _mag->y;
+    mag.z = _mag->z;
+    if(allThree == 0x7) fuseData();
 }
 
 void 
-SensorFusion::receiveAccel() {
-
+SensorFusion::receiveAccel(const geometry_msgs::Vector3::ConstPtr& _acc) {
+    allThree |= 0x2;
+    acc.x = _acc->x;
+    acc.y = _acc->y;
+    acc.z = _acc->z;
+    if(allThree == 0x7) fuseData();
 }
 
 void 
-SensorFusion::receiveGyro() {
-
+SensorFusion::receiveGyro(const geometry_msgs::Vector3::ConstPtr& _gyro) {
+    allThree |= 0x4;
+    gyro.x = _gyro->x/GYRO_HZ;
+    gyro.y = _gyro->y/GYRO_HZ;
+    gyro.z = _gyro->z/GYRO_HZ;
+    if(allThree == 0x7) fuseData();
 }
 
 void
 SensorFusion::fuseData() {
+    allThree = 0;
     // accel and mag
     // normalize data
     vector_normalize(mag);
     vector_normalize(acc);
-    // calc correction vector
-    geometry::Vector3 corMag;
-    vector_corss(dcm[2],mag,corMag);
+    // calc `correction vector
+    geometry_msgs::Vector3 corMag;
+    vector_cross(dcm[2],mag,corMag);
 
-    geometry::Vector3 corAcc;
-    vector_corss(dcm[3],acc,corAcc);
+    geometry_msgs::Vector3 corAcc;
+    vector_cross(dcm[3],acc,corAcc);
     // gyro
     // avg the shit
-    geometry::Vector3 weightedAvg;
-    weightedAvg->x = (gyr->x + ACC_WEIGHT * corAcc->x * MAG_WEIGHT * corMag->x) / (1 + ACC_WEIGHT + MAG_WEIGHT);
-    weightedAvg->y = (gyr->y + ACC_WEIGHT * corAcc->y * MAG_WEIGHT * corMag->y) / (1 + ACC_WEIGHT + MAG_WEIGHT);
-    weightedAvg->z = (gyr->z + ACC_WEIGHT * corAcc->z * MAG_WEIGHT * corMag->z) / (1 + ACC_WEIGHT + MAG_WEIGHT);
+    geometry_msgs::Vector3 weightedAvg;
+    weightedAvg.x = (gyro.x + ACC_WEIGHT * corAcc.x * MAG_WEIGHT * corMag.x) / (1 + ACC_WEIGHT + MAG_WEIGHT);
+    weightedAvg.y = (gyro.y + ACC_WEIGHT * corAcc.y * MAG_WEIGHT * corMag.y) / (1 + ACC_WEIGHT + MAG_WEIGHT);
+    weightedAvg.z = (gyro.z + ACC_WEIGHT * corAcc.z * MAG_WEIGHT * corMag.z) / (1 + ACC_WEIGHT + MAG_WEIGHT);
     DCM_rotate(dcm, weightedAvg);
     // convert to roll pitch and yaw
-    roll = radToDeg(-asin(dcm[2]->x));
-    pitch = radToDeg(atan2(dcm[2]->y, dcm[2]->z));
-    yaw = radToDeg(atan2(dcm[1]->x,dcm[0]->x));
+    roll = radToDeg(-asin(dcm[2].x));
+    pitch = radToDeg(atan2(dcm[2].y, dcm[2].z));
+    yaw = radToDeg(atan2(dcm[1].x,dcm[0].x));
     //heading
-    
-    heading = radToDeg(atan2(mag->x,mag->y);
+    heading = radToDeg(atan2(mag.x,mag.y)) - SYDNEY;
     
     //publish stuff
-
+    
 }
 
 double
-vector_mod(geometry::Vector3 v) {
-    return sqrt( pow(v->x,2) + pow(v->y,2) + pow(v->z,2) );
+SensorFusion::vector_mod(geometry_msgs::Vector3 v) {
+    return sqrt( pow(v.x,2) + pow(v.y,2) + pow(v.z,2) );
 }
 
 void 
-vector_normalize(geometry::Vector3 v) {
+SensorFusion::vector_normalize(geometry_msgs::Vector3 v) {
     double length = vector_mod (v);
-    v->x /= length;
-    v->y /= length;
-    v->z /= length;
+    v.x /= length;
+    v.y /= length;
+    v.z /= length;
 }
 
 void 
-vector_cross(geometry::Vector3 v1, geometry::Vector3 v2, geometry::Vector3 vd) { 
-    v->x = v1->y * v2->z - v1->z * v2->y;   
-    v->y = v1->z * v2->x - v1->x * v2->z;   
-    v->z = v1->x * v2->y - v1->y * v2->x;   
+SensorFusion::vector_cross(geometry_msgs::Vector3 v1, geometry_msgs::Vector3 v2, geometry_msgs::Vector3 vd) { 
+    vd.x = v1.y * v2.z - v1.z * v2.y;   
+    vd.y = v1.z * v2.x - v1.x * v2.z;   
+    vd.z = v1.x * v2.y - v1.y * v2.x;   
 }
 
 double 
-vector_dot(geometry::Vector3 v1, geometry::Vector3 v2) {
-    return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
+SensorFusion::vector_dot(geometry_msgs::Vector3 v1, geometry_msgs::Vector3 v2) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
 void 
-vector_scale(double s, geometry::Vector3 v, geometry::Vector3 vd) {
-    vd->x = s v->x;
-    vd->y = s v->y;
-    vd->z = s v->z;
+SensorFusion::vector_scale(double s, geometry_msgs::Vector3 v, geometry_msgs::Vector3 vd) {
+    vd.x = s * v.x;
+    vd.y = s * v.y;
+    vd.z = s * v.z;
 }
 
 void 
-vector_add(geometry::Vector3 v1, geometry::Vector3 v2, geometry::Vector3 vd) {
-    vd->x = v1->x + v2->x;
-    vd->y = v1->y + v2->y;
-    vd->z = v1->z + v2->z;
+SensorFusion::vector_add(geometry_msgs::Vector3 v1, geometry_msgs::Vector3 v2, geometry_msgs::Vector3 vd) {
+    vd.x = v1.x + v2.x;
+    vd.y = v1.y + v2.y;
+    vd.z = v1.z + v2.z;
 
 }
 
 void 
-DCM_othonormalize(geometry::Vector3 dcm[3]) {
+SensorFusion::DCM_othonormalize(geometry_msgs::Vector3 dcm[3]) {
    double error = vector_dot(dcm[0], dcm[1]);
-   geometry::Vector3 delta[2];
+   geometry_msgs::Vector3 delta[2];
    vector_scale(-error/2, dcm[1], delta[0]);
    vector_scale(-error/2, dcm[0], delta[1]);
    vector_add(dcm[0], delta[0], dcm[0]);
@@ -123,14 +143,14 @@ DCM_othonormalize(geometry::Vector3 dcm[3]) {
 }
 
 void 
-DCM_rotate (geometry::Vector3 dcm[3], geometry::Vector3 v) {
+SensorFusion::DCM_rotate (geometry_msgs::Vector3 dcm[3], geometry_msgs::Vector3 v) {
     int i;
-    geometry::Vector3  dR;
+    geometry_msgs::Vector3  dR;
     for (i = 0; i < 3; ++i) {
         vector_cross(v, dcm[i], dR);
         vector_add(dcm[i], dR, dcm[i]);
     }
-    dcm_orthonormalize(dcm);
+    DCM_othonormalize(dcm);
 }
 
 void
