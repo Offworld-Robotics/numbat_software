baf0677be6b0ca804c291f82c778a3f2a1a8e2ac
hjed <harry@dayfamilyweb.com>
2015-08-28 19:31:10 +1000

    OWRS-133 #time 15m telnet

--- gui/src/owr_wifi_data/script/scrape.py
+++ gui/src/owr_wifi_data/script/scrape.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 import rospy
 import os
-import requests
-import subprocess 
+#import requests
+#import subprocess 
+import telnetlib
 from owr_messages.msg import stream
 
 def run():
-    telnet = subprocess.Popen(["telnet",  "192.168.1.20"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    print telnet.communicate("bluetounge\nbluetoung\n/usr/www/signal.cgi");
+    #telnet = subprocess.Popen(["telnet",  "192.168.1.20"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    #print telnet.communicate("bluetounge\nbluetoung\n/usr/www/signal.cgi");
+    connection = telnetlib.Telnet("192.168.1.21")
+    connection.read_until("login:")
+    connection.write("bluetounge")
+    connection.read_until("Password:")
+    connection.write("bluetoung")
+    connection.read_until("XM.v5.5.8#")
+    connection.write("/usr/www/signal.cgi")
+    json = connection.read_very_eager()
+    print json
+    
     pass
 
     
