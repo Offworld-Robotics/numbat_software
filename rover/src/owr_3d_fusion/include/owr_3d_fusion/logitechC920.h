f2b93859da878d28d352529b8155bc71685404d2
hjed <harry@dayfamilyweb.com>
2015-11-14 16:59:12 +1100

    Start of maths for camera projection in code OWRS-179 OWRS-180 #time 4h #comment lots of maths, and working out c920 camera calibration to produce a small amount of code

---
+++ rover/src/owr_3d_fusion/include/owr_3d_fusion/logitechC920.h
@@ -0,0 +1,35 @@
+/*
+ * Date Started: 14/11/15
+ * Original Author: Harry J.E Day <harry@dayfamilyweb.com>
+ * Editors:
+ * ROS Node Name: camera_fusion_node
+ * ROS Package: owr_3d_fusion
+ * Purpose: Stores #defines to configure the node for use with the logitech c920
+ */
+#ifndef LOGITECH_C920_H
+#define LOGITECH_C920_H
+
+#include <math.h>
+
+//focal length in m
+#define FOCAL_LENGTH_M 0.000367 //3.67mm source: https://forums.logitech.com/t5/Webcams/Focal-Length-of-Logitech-HD-Pro-Webcam-C920/td-p/1280722
+
+//the dimensions of the sensor in meters
+//source: https://forums.logitech.com/t5/Webcams/Logitech-HD-Pro-Webcam-C920-optical-sensor-size/td-p/807013
+#define SENSOR_DIAG_M ((1/3)*0.0244)
+
+
+
+//the aspect ratio of the sensor does not match the aspect ratio of the 1080p output
+//so for now I'm going to assume the pixel size from the sensor width
+
+#define RESOLUTION_W 1920
+#define RESOLUTION_H 1080
+
+// #define PIXEL_TO_M_RATIO (SENSOR_WIDTH_M/RESOLUTION_W)
+// #define M_TO_PIXEL_RATIO (RESOLUTION_W/SENSOR_WIDTH_M)
+#define PX_TO_M SENSOR_DIAG_M/sqrt(pow(1920,2) + pow(1080,2))
+#define M_TO_PX sqrt(pow(1920,2) + pow(1080,2))/SENSOR_DIAG_M/
+#define FOCAL_LENGTH_P (FOCAL_LENGTH_M * M_TO_PX)
+
+#endif
--- rover/src/owr_3d_fusion/src/CameraFusionNode.cpp
+++ rover/src/owr_3d_fusion/src/CameraFusionNode.cpp
@@ -8,6 +8,7 @@
  */
 
 #include "owr_3d_fusion/CameraFusionNode.h"
+#include "owr_3d_fusion/logitechC920.h"
 
 
 int main(int argc, char ** argv) {
@@ -28,14 +29,35 @@ sensor_msgs::PointCloud CameraFusionNode::doColouring ( pcl::PointCloud< pcl::Po
 }
 
 pcl::PointCloud< pcl::PointXYZRGB > CameraFusionNode::getLatestPointCloud() {
+    
     return pcl::PointCloud<pcl::PointXYZRGB>();
 }
 
 void CameraFusionNode::imageCallback ( const sensor_msgs::Image::ConstPtr& frame ) {
-
+    pcl::PointCloud<pcl::PointXYZRGB> pc = getLatestPointCloud();
+    int x,y;
+    float deltaX, deltaY;
+    const float focalLengthPx = (PIXEL_TO_M_RATIO/FOCAL_LENGTH_M);
+    for(x = 0; x < frame->width; x++) {
+        deltaX = tanh(x*focalLengthPx);
+        for(y = 0; y < frame->height; y++) {
+            deltaY = tanh(y*focalLengthPx);
+            //gradient of z is the minimum resolution on the z axis of the point cloud
+            float px, py, pz;
+            float dist = 0;
+#define RES 1
+            //TODO: 
+            for(dist = 0;dist < 100; dist+=RES) {
+                px = deltaX * RES;
+                py = deltaY * RES;
+                pz = dist;
+                pc.at(px,py,px);
+            }
+        }
+    }
 }
 
 void CameraFusionNode::pointCloudCallback ( const sensor_msgs::PointCloud::ConstPtr& pc ) {
-
+    
 }
 
