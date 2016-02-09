4810b9b9db95afd879cf5e0d1822b42835e2603d
Harry J.E Day <harry@dayfamilyweb.com>
2016-02-09 22:15:48 +1100

    OWRS-203 #time 25m transcribed pseduo code to C++ for steering algorithm, added to basic ros node and compiled. Next step is to set up the spinning and test with simulator

--- rover/src/owr_drive_controls/src/cmdVelToJoints.cpp
+++ rover/src/owr_drive_controls/src/cmdVelToJoints.cpp
@@ -15,7 +15,7 @@
     HALF_ROVER_WIDTH_X := 271.30mm == .27130 (distance from base_link_origin to back_right_wheel shaft)
     FRONT_W_2_BACK_W_X := 542.16mm == 0.54216
 */
-#define ROVER_CENTER_2_WHEEL_Y 0.40023
+#define ROVER_CENTRE_2_WHEEL_Y 0.40023
 #define BACK_WHEEL_SPAN 0.80273
 #define HALF_ROVER_WIDTH_X .27130
 #define FRONT_W_2_BACK_W_X 0.54216
@@ -25,19 +25,64 @@
 
 #include <geometry_msgs/Vector3.h>
 
-CmdVelToJoints::CmdVelToJoints() {
+int main(int argc, char ** argv) {
+    ros::init(argc, argv, "owr_telop");
+    CmdVelToJoints CmdVelToJoints;
+    //CmdVelToJoints.run();
+}
 
+CmdVelToJoints::CmdVelToJoints() {
+     ros::TransportHints transportHints = ros::TransportHints().tcpNoDelay();
+     cmdVelSub = nh.subscribe<geometry_msgs::Twist>(TOPIC,1, &CmdVelToJoints::reciveVelMsg , this, transportHints);
 }
 
 //for a full explanation of this logic please see the scaned notes at
 //https://bluesat.atlassian.net/browse/OWRS-203
 void CmdVelToJoints::reciveVelMsg ( const geometry_msgs::Twist::ConstPtr& velMsg ) {
-    const double turnAngle = atan(velMsg.linear.x/velMsg.linear.y);
+    const double turnAngle = atan(velMsg->linear.x/velMsg->linear.y);
     const double rotationRadius = HALF_ROVER_WIDTH_X/sin(turnAngle);
     geometry_msgs::Vector3 rotationCentre;
     rotationCentre.x = -HALF_ROVER_WIDTH_X;
     rotationCentre.y = sqrt(pow(rotationRadius,2)+pow(HALF_ROVER_WIDTH_X,2));
-    const double angularVelocity = velMsg.linear.x / rotationRadius;
+    const double angularVelocity = velMsg->linear.x / rotationRadius;
+    
+    //calculate the radiuses of each wheel about the rotation center
+    //NOTE: if necisary this could be optimised
+    double closeBackR = fabs(rotationCentre.y - ROVER_CENTRE_2_WHEEL_Y);
+    double farBackR = fabs(rotationCentre.y + ROVER_CENTRE_2_WHEEL_Y);
+    double closeFrontR = sqrt(pow(closeBackR,2) + pow(FRONT_W_2_BACK_W_X,2));
+    double farFrontR = sqrt(pow(farBackR,2) + pow(FRONT_W_2_BACK_W_X,2));
+    
+    //V = wr
+    double closeBackV = closeBackR * angularVelocity;
+    double farBackV = farBackR * angularVelocity;
+    double closeFrontV = closeFrontR * angularVelocity;
+    double farFrontV = farFrontR * angularVelocity;
+    
+    //work out the front wheel angles
+    double closeFrontAng = 90*(2/M_PI)-atan(closeBackR/FRONT_W_2_BACK_W_X);
+    double farFrontAng = 90*(2/M_PI)-atan(farBackR/FRONT_W_2_BACK_W_X);
+    
+    double frontLeftMotorV, frontRightMotorV, backLeftMotorV, backRightMotorV;
+    double frontLeftAng, frontRightAng;
+    
+    //work out which side to favour
+    if(0 <= turnAngle && turnAngle <= 180*(2/M_PI)) {
+        frontLeftMotorV = closeFrontV;
+        backLeftMotorV = closeBackV;
+        frontRightMotorV = farFrontV;
+        backRightMotorV = farBackV;
+        frontLeftAng = closeFrontAng;
+        frontRightAng = farFrontAng;
+    } else {
+        frontRightMotorV = closeFrontV;
+        backRightMotorV = closeBackV;
+        frontLeftMotorV = farFrontV;
+        backLeftMotorV = farBackV;
+        frontLeftAng = farFrontAng;
+        frontRightAng = closeFrontAng;
+    }
+    
     
     
 }
