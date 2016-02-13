c1eb3dd7933ab24a77e4ccd3e89cdc81e7853adf
YiweiHan <yiweih@live.com>
2016-02-13 16:24:38 +1100

    OWRS-156 #time 2h transforms proof of concept working, main code in ArmWidgetGUI.cpp

--- gui/src/owr_gui/src/ArmWidgetGUI.cpp
+++ gui/src/owr_gui/src/ArmWidgetGUI.cpp
@@ -4,10 +4,15 @@
 #include <cstring>
 #include <list>
 #include <limits>
+#include <iostream>
 #include "ArmWidgetGUI.h"
 #include "ArmWidgetNode.h"
 #include <ros/ros.h>
 #include <sensor_msgs/NavSatFix.h>
+
+#include <tf/transform_listener.h>
+
+using namespace std;
 
 void ArmWidgetGUI::idle() {
 	display();
@@ -42,8 +47,30 @@ ArmWidgetGUI::ArmWidgetGUI(int width, int height, int *argc, char *argv[]) : GLU
 
 int main(int argc, char *argv[]) {
 	ros::init(argc, argv, "ArmWidgetGUI");
-	ArmWidgetGUI gui(1855, 1056, &argc, argv);
-	printf("Running!\n");
-	gui.run();
+	//ArmWidgetGUI gui(1855, 1056, &argc, argv);
+	//gui.run();
+	
+	ros::NodeHandle node;
+	tf::TransformListener listener;
+
+	ros::Rate rate(10.0);
+	while (node.ok()){
+		tf::StampedTransform transform;
+		try {
+			//listener.lookupTransform("/from_frame", "/to_frame", ros::Time(0), transform);
+			listener.lookupTransform("/base_link", "/top_actuator_shaft", ros::Time(0), transform);
+			cout << "New tf\n";
+			cout << transform.getOrigin().x() << endl;
+			cout << transform.getOrigin().y() << endl;
+			cout << transform.getOrigin().z() << endl << endl;
+		}
+		catch (tf::TransformException &ex) {
+			ROS_ERROR("%s",ex.what());
+			ros::Duration(1.0).sleep();
+			continue;
+		}
+
+		rate.sleep();
+	}
 	return 0;
 }
