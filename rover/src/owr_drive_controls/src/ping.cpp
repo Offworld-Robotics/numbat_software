7f903c73f1e3509b0e0d3c40b65a64c8b6341ce4
brakespear <cbrakespear@hotmail.com.au>
2016-04-09 12:54:27 +1000

    OWRS-224 #time 30min began working on ssh but unsure of how to test it

--- rover/src/owr_drive_controls/src/ping.cpp
+++ rover/src/owr_drive_controls/src/ping.cpp
@@ -11,15 +11,21 @@
 #include <std_msgs/Bool.h>
 #include <limits>
 
+
 int main(int argc, char ** argv) {
     
     
     //init ros
     ros::init(argc, argv, "owr_ping_node");
     ros::NodeHandle n;
+    n.setParam("groundStation","192.168.1.3");
     ros::Publisher ping_pub = n.advertise<std_msgs::Bool>("owr/ping",10);
     while (ros::ok()){
         std_msgs::Bool networkStatus;
+        char* groundStation;
+        n.getParam("groundStation",groundStation);
+        ssh groundStation;
+        ping groundStation;
         networkStatus.data = true; //TODO:for now just constantly claim the network status is good. 
         ping_pub.publish(networkStatus);
         ros::spinOnce();
