d7e5183ad1888472e4e5583a5748e5ebbf0a0fee
Nuno Das Neves <ndasneves@gmail.com>
2016-04-09 12:49:09 +1000

    OWRS-194 #time 3h It's working!

--- rover/src/owr_auton_pathing/include/A-star.h
+++ rover/src/owr_auton_pathing/include/A-star.h
@@ -3,76 +3,89 @@
 #ifndef A-STAR_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <cmath>
-#include <boost/concept_check.hpp>
+#include <cmath>        // pow and stuff
+#include <vector>       // vector
+#include <queue>        // priority_queue
+#include <algorithm>        // find
 
-class aStar {
-private:
-  std::vector<std::vector<double>> occupancyGrid; //will change to matrix
-  std::vector<point> closedSet;
-  typedef std::priority_queue<point, std::vector<point>, comparePoints> openSet;
-  std::vector<point> finalPath;
-  point goal;
-  point start;
-  point currentPos;
-public:
-  double getDist(point point1, point point2);	// Euclidean distance between 2 points
-  double getH(point point1, point point2);	// Not used yet
-  void getF(point &point1);			// Gets all the important stuff for a given point (cost, weight, path...)
-  void getNeighbors(point point1);		// Get the neighbors of a point, push to openSet, get weight etc
-  void findPath();				// Main function
-  void getPath();				// Reconstruct path back to start
-  aStar() {
-    occupancyGrid.resize(20);
-    for(unsigned int i = 0; i < occupancyGrid.size(); ++i) {
-      occupancyGrid[i].resize(20);
-      for(unsigned int j = 0; j < occupancyGrid[i].size(); ++j) {
-	occupancyGrid[i][j]=0; // change to random 0-1 later
-      }
-    }
-  }
-  class point {
+class point {
   public:
     int x;
     int y;
-    bool isStart;		// is the start
-    bool isGoal;		// is the goal
-    double weight;		// f = g + h
-    double goalDist;		// distance from this point to the goal
-    double stepCost;		// cost to move to this point from any adjacent point
-    double cost;		// total cost to get to this point from the start
-    point * previous;		// [x,y] of previous point
-    
-    // I forget what this does
+    bool isStart;       // is the start
+    bool isGoal;        // is the goal
+    double weight;      // f = g + h
+    double goalDist;    // distance from this point to the goal
+    double stepCost;    // cost to move to this point from any adjacent point
+    double cost;        // total cost to get to this point from the start
+    point* previous;    // pointer to previous point NOT USED CURRENTLY
+    long previndex;     // index of closedSet which is the previous point (better than using a pointer right now...)
+
+    // constructor function; sets default values
     point(int _x=0, int _y=0, bool _isStart=false, bool _isGoal=false, double _weight=0.0f, double _goalDist=0.0f, 
-	  double _stepCost=0.0f, double _cost=0.0f, point * _previous=nullptr) {
-      x = _x;
-      y = _y;
-      isStart = _isStart;
-      isGoal = _isGoal;
-      weight = _weight;
-      goalDist = _goalDist;
-      stepCost = _stepCost;
-      cost = _cost;
-      previous = _previous;
+    double _stepCost=0.0f, double _cost=0.0f, point *_previous=nullptr, long _previndex=-1) {
+        x = _x;
+        y = _y;
+        isStart = _isStart;
+        isGoal = _isGoal;
+        weight = _weight;
+        goalDist = _goalDist;
+        stepCost = _stepCost;
+        cost = _cost;
+        previous = _previous;
+        previndex = _previndex;
     }
-    friend operator==(point lhs, point rhs);
-    friend operator!=(point lhs, point rhs);
-  };
-  class comparePoints {
+    //friend bool operator==(point lhs, point rhs);
+    //friend bool operator!=(point lhs, point rhs);
+
+};
+bool operator==(const point& lhs, const point& rhs){
+    return (lhs.x == rhs.x && lhs.y == rhs.y);
+}
+bool operator!=(const point& lhs, const point& rhs){
+    return (lhs.x != rhs.x || lhs.y != rhs.y);
+}
+
+class aStar {
+    class comparePoints {
+      public:
+            comparePoints(){
+        }
+        bool operator() (const point& lhs, const point& rhs) const
+        {
+            return lhs.weight > rhs.weight;
+        }
+    };
+  private:
+    // -1 = impassable
+    std::vector<std::vector<double> > occupancyGrid; // will change to matrix maybe?
+    std::vector<point> closedSet;
+    typedef std::priority_queue<point, std::vector<point>, comparePoints> comparePointsQueue;
+    comparePointsQueue openSet;
+    std::vector<point> finalPath;
+    point goal;
+    point start;
+    point currentPos;
   public:
-    comparePoints();
-    bool operator() (const point& lhs, const point& rhs) const
-    {
-      return lhs.cost < rhs.cost;
+    double getDist(point point1, point point2); // Euclidean distance between 2 points
+    void getF(point &point1);                   // Gets all the important stuff for a given point (cost, weight, path...)
+    void getNeighbors(point point1);            // Get the neighbors of a point, push to openSet, get weight etc
+    void findPath();                            // Main function
+    void getPath();                             // Reconstruct path back to start
+    void printGrid();                           // TEsT print whole grid and any other relevant stuff
+    aStar(comparePointsQueue _openSet, std::vector<std::vector<double>> _occupancyGrid, std::vector<point> _closedSet, std::vector<point> _finalPath, point _goal, point _start, point _currentPos) {
+        occupancyGrid = _occupancyGrid;
+        openSet = _openSet;
+        closedSet = _closedSet;
+        finalPath = _finalPath;
+        goal = _goal;
+        start = _start;
+        currentPos = _currentPos;
     }
-  };
+    aStar() {
+
+    }
+
 };
 
-aStar::point::operator==(point lhs, point rhs){
-  return (lhs.x == rhs.x && lhs.y == rhs.y);
-}
-aStar::point::operator!=(point lhs, point rhs){
-  return !aStar::point::operator==(lhs, rhs);
-}
 #endif
--- rover/src/owr_auton_pathing/src/A-star.cpp
+++ rover/src/owr_auton_pathing/src/A-star.cpp
@@ -1,88 +1,114 @@
 /* A Star search
  * By Nuno Das Neves
- * Date 5/3/2016
+ * Date 5/3/2016 - 19/3/2016
  * A* path plan based off LIDAR point-cloud
  */
 
+#include <iostream>
 #include "A-star.h"
-#include <boost/concept_check.hpp>
 
+/*
+ * Instructions:
+ * Create an aStar object
+ * Give it an occupancyGrid std::vector<std::vector<double> > -1 = impassable
+ * set start.x and start.y
+ * set goal.x and goal.y
+ * run findPath()
+ * finalPath now contains the path hopefully
+ */
+
+
+//------------------------test stuff only here
+
+void aStar::printGrid(){   
+    // this is how these loops should be set up for correct indexing (pretty sure) i = x; j = y
+    for(unsigned int j = 0; j < occupancyGrid[0].size(); ++j) {
+      for(unsigned int i = 0; i < occupancyGrid.size(); ++i) {
+        point printPoint(i,j);
+        if (start.x == i && start.y == j) {
+            std::cout << "S ";      // S for start
+        } else if (goal.x == i && goal.y == j) {
+            std::cout << "G ";      // G for goal
+        } else if (occupancyGrid[i][j] == -1) {
+            std::cout << "B ";      // B for block
+        } else if(std::find(finalPath.begin(), finalPath.end(), printPoint) != finalPath.end()){
+            std::cout << "# ";      // # for final path
+        } else {
+            std::cout << "- ";      // - for empty (passable) grid square
+        }
+      }
+      std::cout << std::endl;
+    }
+}
+
+//-------------------------end test stuff
 
 void aStar::findPath() {
-  
-  // example values
-  start.x=0;
-  start.y=0;
-  start.cost = 0;
-  start.isStart = true;
-  goal.x=20;
-  goal.y=20;
-  goal.isGoal = true;
-  
-  currentPos = start;			// set our current position to the start position
-  closedSet.push(currentPos);		// add current position to the closed (explored) set
-  
-  while(currentPos != goal) {
-    
-    getNeighbors(currentPos); 		// get neighbors of current position
-    closedSet.push(openSet.top());	// push the top member of openSet to closedSet
-    openSet.pop();			// 
-    currentPos = openSet.top(); 	// set current position to the next in the priority queue
-    
-    if(currentPos == goal) {		// if we're at the goal
-      getPath();
-      break;
-    } else if(openSet.empty()) {		// or if there is no solution (no more valid points)
-      printf("no solution");
-      break;
+    currentPos = start;           // set our current position to the start position
+    while(currentPos != goal) {
+        closedSet.push_back(currentPos);    // push the current point into closedSet
+        getNeighbors(currentPos);       // get neighbors of current position
+        currentPos = openSet.top();     // set current position to the next in the priority queue
+        openSet.pop();
+        if(currentPos == goal) {        // if we're at the goal
+            getPath();
+            break;
+        } else if(openSet.empty()) {        // or if there is no solution (no more valid points)
+            break;
+        }
     }
-  }
 }
 
 void aStar::getNeighbors (point point1) {
-  point adjacent;
-  for(unsigned int i = point1.x-1; i <= point1.x+1; ++i) {
-    for(unsigned int j = point1.y-1; j <= point1.y+1; ++j) {
-      // if point1 is in the closedSet
-      if(std::find(closedSet.begin(), closedSet.end(), point1) != closedSet.end()) {
-	continue;
-      } else {
-	if (!(i == point1.x && j==point1.y)){ 		//we don't want to push the current point!
-	  adjacent.stepCost = aStar::occupancyGrid[i][j];
-	  adjacent.x = i;
-	  adjacent.y = j;
-	  if (adjacent.stepCost < 1){				// make sure its passable before using resources on it
-	    getF(adjacent);					// get the F cost etc
-	    openSet.push(adjacent);				//
-	  } else {
-	    // if the point is impassible, push it to the closedSet (we never want to expand it)
-	    closedSet.push(adjacent);
-	  }
-	}
-      }
+    point adjacent;
+    for(int i = point1.x-1; ((i <= point1.x+1) && (i<=9)); ++i) {
+        adjacent.x = i;
+        for(int j = point1.y-1;((j <= point1.y+1) && (j<=9)); ++j) {
+            adjacent.y = j;
+            if((std::find(closedSet.begin(), closedSet.end(), adjacent) != closedSet.end()) || (i<0) || (j<0)) {
+                continue; // if point1 is in the closedSet we don't want to expand it; continue
+            } else {
+                if ((point1.x == i) || (point1.y == j)) {
+                    adjacent.stepCost = occupancyGrid[i][j];    // if adjacent, use normal stepCost
+                } else {
+                    // ---this section checks blocks next to adjacent and point1 to see if they're blocked; if both are then it treats adjacent as impassable
+                    int xset = adjacent.x+(point1.x - adjacent.x);
+                    int yset = adjacent.y+(point1.y - adjacent.y);
+                    if ((xset >= 0 && xset <=9) && (yset >= 0 && yset <=9)) {
+                        if ((occupancyGrid[xset][adjacent.y] == -1) && (occupancyGrid[adjacent.x][yset] == -1)) {
+                            continue;   //not impassable but blocked by adjacent points
+                        }
+                    }
+                    // ---otherwise just add the diagonal stepCost
+                    adjacent.stepCost = occupancyGrid[i][j]*sqrt(2);    // if diagonal, multiply stepCost by sqrt(2) because that's how geometry works
+                }
+                if (adjacent.stepCost >= 0){                    // make sure its passable before using resources on it
+                    getF(adjacent);                             // get the F cost etc
+                    openSet.push(adjacent);                     // push it to the openSet for future expansion
+                } else {
+                    // if the point is impassible, push it to the closedSet (we never want to expand it)
+                    closedSet.push_back(adjacent);
+                }
+            }
+        }
     }
-  }
 }
 
 void aStar::getF (point &point1) {
-  point1.goalDist = getDist(point1, goal);		// get distance to goal (our only heuristic right now)
-  point1.cost = currentPos.cost + point1.stepCost; 	// get the total cost up to this point
-  point1.weight =  point1.cost + point1.goalDist; 	// f = g + h
-  point1.previous = &currentPos;
+    point1.goalDist = getDist(point1, goal);      // get distance to goal (our only heuristic right now)
+    point1.cost = currentPos.cost + point1.stepCost;  // get the total cost up to this point
+    point1.weight =  point1.cost + point1.goalDist;   // f = g + h
+    point1.previndex = std::find(closedSet.begin(), closedSet.end(), currentPos) - closedSet.begin(); // returns index of a point matching x,y of currentPos in closedSet
 }
 
 double aStar::getDist (point point1, point point2) {
-  double dist = sqrt(pow((point1.x - point2.x), 2) + pow((point1.y - point2.y), 2)); // euclidean distance
-  return dist;
+    double dist = sqrt(pow((point1.x - point2.x), 2) + pow((point1.y - point2.y), 2)); // euclidean distance
+    return dist;
 }
 
 void aStar::getPath() {
-  while(currentPos.previous != nullptr){
-    finalPath.push_back(currentPos);
-    currentPos = currentPos->previous;
-  }
-}
-
-double aStar::getH (point point1, point point2) {
-  
+    while(currentPos.previndex != -1){
+        finalPath.push_back(currentPos);
+        currentPos = closedSet[currentPos.previndex];
+    }
 }
