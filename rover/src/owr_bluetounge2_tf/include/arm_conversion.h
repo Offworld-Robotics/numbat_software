9dd5c56ec0803fcc2b967cbb30cf9b433e9c192c
linux <chris.squire@hotmail.com>
2016-04-23 16:14:03 +1000

    OWRS-213 #time 5h Implementation completed, need to build angle lookup table using CAD

--- rover/src/owr_bluetounge2_tf/include/arm_conversion.h
+++ rover/src/owr_bluetounge2_tf/include/arm_conversion.h
@@ -4,26 +4,35 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
-#include <tf/transform_broadcaster.h>
+
+#define RESOLUTION 0.005
+#define LOWER_ACTUATOR_LIMIT 0.252
+#define UPPER_ACTUATOR_LIMIT 0.199 
+#define UPPER_STROKE_NAME "upper_actuator"
+#define LOWER_STROKE_NAME "lower_actuator"
+
+
+static double angle_list[0][2006] = {};
 
 class conversion {
 private:
     ros::NodeHandle n;
     ros::Subscriber actuator_sub;
     ros::Publisher js_pub;
-    tf::TransformBroadcaster broadcaster;
-    std::vector<std::map<std::string, double>> inPositions;
-    std::vector<std::map<std::string, double>> outPositions
+
+    std::map<std::pair<double, double>, std::pair<double, double> > position_map;
+    sensor_msgs::JointState angle_msgs;
+    std::vector<std::pair<std::string, double> > inPositions;
 
 public:
+    void actuatorCallBack(const sensor_msgs::JointState::ConstPtr& actuators);
+    void performTransform();
+    void initialiseMap();
     conversion() {
         inPositions.resize(2);
-        outPositions.resize(42); //random number
-        actuator_sub = n.subscribe<sensor_msgs::joint_state()
-        js_pub = n.advertise<sensor_msgs::joint_state>();
+        actuator_sub = n.subscribe<sensor_msgs::JointState>("owr/arm_angle", 1, &conversion::actuatorCallBack, this);
+        js_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
     }
-    void actuatorCallBack(const sensor_msgs::joint_state::ConstPtr& actuators);
-    void performTransform();
-}
+};
 
 #endif
--- rover/src/owr_bluetounge2_tf/src/arm_conversion.cpp
+++ rover/src/owr_bluetounge2_tf/src/arm_conversion.cpp
@@ -9,32 +9,96 @@
 */
 
 
+//I'm going to do this slightly different, I'm going to create a hashtable that relates actuator positions
+//to main arm angle, the other angles are kind of pointless, it'll be in arm_convserion.h
+
 int main() 
 {
-    ros::init(argc, argv, "arm_conversion");
-    ros::NodeHandle n;
-    ros::Subscriber actuator_sub = n.subsrciber("actuators", 1, actuatorCallBack);
-    ros::Publisher js_pub = n.advertise<sensor_msgs::joint_state>("arm_js", 1);
-
+    ros::Rate r(100); //100 hz, 1/100th second    
+    conversion Converter;
+    Converter.initialiseMap();
     while(ros::ok()) 
     {
-
+        ros::spin();
+        r.sleep();
     }
+    
 }
 
 //this function will call a transform function, and then a publish function
-void actuatorCallBack(const sensor_msgs::joint_state::ConstPtr& actuators)
+void conversion::actuatorCallBack(const sensor_msgs::JointState::ConstPtr& actuators)
 {
-    std::vector<double> positions;
-    positions.resize(2);    
-    if(actuators->name != " ") {
-        positions[0] = msg->position[0];
-        positions[1] = msg->position[1];    
+    if(actuators->name[0] == UPPER_STROKE_NAME) {
+    	inPositions[0] = std::make_pair(actuators->name[0], actuators->position[0]);
+    	inPositions[1] = std::make_pair(actuators->name[1], actuators->position[1]);
     } else {
-        positions[1] = msg->position[0];
-        positions[0] = msg->position[1];
+        inPositions[0] = std::make_pair(actuators->name[1], actuators->position[1]);
+        inPositions[1] = std::make_pair(actuators->name[0], actuators->position[0]);
     }
-    performTransform(positions)
+    performTransform();
 }
 
+void conversion::performTransform() {
+   
+    //idea:
+    //work out the closet predefined position
+    //(Can do with modulus) and then:
+    //initially, snap it to position
+    //eventually assume linearity and determine position 
 
+    //find first closest
+    double extent1 = floor(inPositions[0].second/RESOLUTION); //gives an index
+    double extent2 = floor(inPositions[1].second/RESOLUTION);
+
+ 
+    //1m = 100cm
+    //1cm = 10mm
+    //1m = 1000mm 
+    if(fmod(inPositions[0].second, RESOLUTION) > 0.0025) {
+        extent1 = (extent1+1)*RESOLUTION;
+    } else {
+        extent1 = extent1*RESOLUTION;
+    }
+
+    if(fmod(inPositions[1].second,RESOLUTION) > 0.0025) {
+        extent2 = (extent2+1)*RESOLUTION;
+    } else {
+        extent2 = extent2*RESOLUTION;
+    }
+
+
+    std::pair<double,double> link_angles = position_map.at(std::make_pair<double, double>(extent1, extent2)); 
+    
+    //pass angle into a joint state message & send it on
+    angle_msgs.name.push_back("arm_base_link_joint");
+    angle_msgs.name.push_back("arm_link_link_joint");
+    angle_msgs.position.push_back(link_angles.first);
+    angle_msgs.position.push_back(link_angles.second);
+
+    angle_msgs.velocity.push_back(0.0);
+    angle_msgs.velocity.push_back(0.0);
+
+    angle_msgs.effort.push_back(0.0);    
+    angle_msgs.effort.push_back(0.0);
+   
+    js_pub.publish(angle_msgs); 
+    ros::Subscriber actuator_sub;
+    ros::Publisher js_pub;
+
+}
+
+void conversion::initialiseMap() {
+   
+    int index = 0;
+    double upper_stroke = 0.0;
+    double lower_stroke = 0.0;
+    
+    for(double upper_stroke = 0.0; upper_stroke < UPPER_ACTUATOR_LIMIT; upper_stroke+=RESOLUTION) {
+        for(double lower_stroke = 0.0; lower_stroke < LOWER_ACTUATOR_LIMIT; lower_stroke+=RESOLUTION) {
+            std::pair<double, double> strokes = std::make_pair(upper_stroke, lower_stroke);
+            std::pair<double, double> angles = std::make_pair(angle_list[0][index], angle_list[1][index]);
+            position_map.insert(std::make_pair<std::pair<double, double>, std::pair<double, double> >(strokes, angles));
+        }
+    }
+
+}
