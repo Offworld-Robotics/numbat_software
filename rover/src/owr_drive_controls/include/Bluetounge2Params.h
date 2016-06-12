bff40fc7ab58223bbe69fecd30ccf9e8fedcd629
Harry J.E Day <harry@dayfamilyweb.com>
2016-06-12 21:41:26 +1000

    OWRS-237 #time 10m correct gear ratio for arm rotator

--- rover/src/owr_drive_controls/include/Bluetounge2Params.h
+++ rover/src/owr_drive_controls/include/Bluetounge2Params.h
@@ -18,7 +18,7 @@ const double SWERVE_GEARS[] = {1.0/455.0, -1.0/4.75};
 #define SWERVE_MOTOR_RPM (26*455) 
 #define SWERVE_RADIUS 0.12 //TODO
 
-const double ARM_BASE_ROTATE_GEARS[] = {1.0/455.0, 1}; //TODO: put not stupid values here
+const double ARM_BASE_ROTATE_GEARS[] = {1.0/455.0, 4.0/25.0}; //TODO: put not stupid values here
 #define ARM_BASE_ROTATE_N_GEARS 2
 #define ARM_BASE_ROTATE_MOTOR_MAX_PWM 2000
 #define ARM_BASE_ROTATE_MOTOR_MIN_PWM 1000
