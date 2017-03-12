7efa0a237578b1c836bd800235e64b0c33a5c708
YiweiHan <yiweih@live.com>
2017-03-12 13:54:18 +1100

    OWRS-300 #time 3h now calculates twist averages for each camera

--- rover/src/owr_positioning/include/optical_localization.h
+++ rover/src/owr_positioning/include/optical_localization.h
@@ -3,32 +3,48 @@
 
 #include <ros/ros.h>
 #include <opencv2/imgproc.hpp>
+#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/TwistWithCovarianceStamped.h>
+#include <ecl/threads.hpp>
 
-#define NUM_CAMS 2U
+#define NUM_CAMS 4U
 
 class optical_localization {
     private:
+        ros::Publisher pub;
+        
         ros::Subscriber sub[NUM_CAMS];
-        ros::Publisher pub[NUM_CAMS];
         
         cv::Mat prev_gray[NUM_CAMS];
-        cv::Mat translation_current[NUM_CAMS];
-        double scale_x[NUM_CAMS];
-        double scale_y[NUM_CAMS];
-        double rotation[NUM_CAMS];
         
         ros::Time prev_time[NUM_CAMS];
         
         double pixels_per_metre_traversed[NUM_CAMS];
         double rotation_velocity_scale[NUM_CAMS];
         
-        geometry_msgs::TwistWithCovarianceStamped my_twist[NUM_CAMS];
+        bool is_first[NUM_CAMS];
+        
+        // most recent calculated twist for each camera
+        geometry_msgs::Twist most_recent[NUM_CAMS];
+        
+        // most recent averaged twist for each camera
+        geometry_msgs::Twist most_recent_averages[NUM_CAMS];
+        
+        // the final twist to be published
+        geometry_msgs::TwistWithCovarianceStamped my_twist;
+        
+        ecl::Mutex mutex;
         
         void image_callback0(const sensor_msgs::Image::ConstPtr& image);
         void image_callback1(const sensor_msgs::Image::ConstPtr& image);
+        void image_callback2(const sensor_msgs::Image::ConstPtr& image);
+        void image_callback3(const sensor_msgs::Image::ConstPtr& image);
         
         void process_image(const sensor_msgs::Image::ConstPtr& image, const unsigned int idx);
+        
+        geometry_msgs::Twist average(const geometry_msgs::Twist &first, const geometry_msgs::Twist &second);
+        
+        geometry_msgs::Twist getVectorSum();
         
     public:
         optical_localization(int argc, char *argv[]);
--- rover/src/owr_positioning/src/optical_localization.cpp
+++ rover/src/owr_positioning/src/optical_localization.cpp
@@ -3,10 +3,40 @@
 #include <algorithm>
 #include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
+#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/TwistWithCovarianceStamped.h>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/video/tracking.hpp>
+#include <ecl/threads.hpp>
 #include "optical_localization.h"
+
+geometry_msgs::Twist optical_localization::average(const geometry_msgs::Twist &first, const geometry_msgs::Twist &second) {
+    geometry_msgs::Twist result;
+    result.linear.x = (first.linear.x + second.linear.x) / 2.0;
+    result.linear.y = (first.linear.y + second.linear.y) / 2.0;
+    result.angular.z = (first.angular.z + second.angular.z) / 2.0;
+    return result;
+}
+
+geometry_msgs::Twist optical_localization::getVectorSum() {
+    geometry_msgs::Twist result;
+    
+    // TODO do actual vector sum
+    result.linear.x = 0;
+    result.linear.y = 0;
+    result.angular.z = 0;
+    
+    for(unsigned int i = 0;i < NUM_CAMS;++i) {
+        result.linear.x += most_recent_averages[i].linear.x;
+        result.linear.y += most_recent_averages[i].linear.y;
+        result.angular.z += most_recent_averages[i].angular.z;
+    }
+    result.linear.x /= NUM_CAMS;
+    result.linear.y /= NUM_CAMS;
+    result.angular.z /= NUM_CAMS;
+    
+    return result;
+}
 
 void optical_localization::process_image(const sensor_msgs::Image::ConstPtr& image, const unsigned int idx) {
     cv::Mat frame = cv_bridge::toCvCopy(image)->image;
@@ -21,11 +51,6 @@ void optical_localization::process_image(const sensor_msgs::Image::ConstPtr& ima
             double scale_y_delta = norm(affineTransform.col(1));
             double rotation_delta = atan2(affineTransform.at<double>(1,1), affineTransform.at<double>(0,1)) - M_PI/2.0;
             
-            translation_current[idx] += translation_delta;
-            scale_x[idx] *= scale_x_delta;
-            scale_y[idx] *= scale_y_delta;
-            rotation[idx] += rotation_delta;
-            
             double time_scale = 0;
             ros::Time current_time = ros::Time::now();
             if(!prev_time[idx].isZero()) {
@@ -33,19 +58,33 @@ void optical_localization::process_image(const sensor_msgs::Image::ConstPtr& ima
             }
             prev_time[idx] = current_time;
             
-            my_twist[idx].twist.twist.linear.x = translation_delta.at<double>(0) * time_scale / pixels_per_metre_traversed[idx];
-            my_twist[idx].twist.twist.linear.y = translation_delta.at<double>(1) * time_scale / pixels_per_metre_traversed[idx];
-            my_twist[idx].twist.twist.angular.z = rotation_delta * rotation_velocity_scale[idx] * time_scale;
-            my_twist[idx].header.stamp = current_time;
+            geometry_msgs::Twist this_twist;
+            this_twist.linear.x = translation_delta.at<double>(0) * time_scale / pixels_per_metre_traversed[idx];
+            this_twist.linear.y = translation_delta.at<double>(1) * time_scale / pixels_per_metre_traversed[idx];
+            this_twist.angular.z = rotation_delta * rotation_velocity_scale[idx] * time_scale;
             
-            pub[idx].publish(my_twist[idx]);
+            if(is_first[idx]) {
+                is_first[idx] = false;
+                most_recent_averages[idx] = this_twist;
+            } else {
+                most_recent_averages[idx] = average(most_recent[idx], this_twist);
+            }
+            most_recent[idx] = this_twist;
+            
+            // TODO enable multithreading
+            mutex.lock();
+            my_twist.twist.twist = getVectorSum();
+            my_twist.header.stamp = ros::Time::now();
+            
+            pub.publish(my_twist);
+            ++my_twist.header.seq;
             
             ROS_INFO_STREAM("CAM " << idx);
-            ROS_INFO_STREAM("lin x: " << my_twist[idx].twist.twist.linear.x);
-            ROS_INFO_STREAM("lin y: " << my_twist[idx].twist.twist.linear.y);
-            ROS_INFO_STREAM("ang z: " << my_twist[idx].twist.twist.angular.z);
+            ROS_INFO_STREAM("lin x: " << my_twist.twist.twist.linear.x);
+            ROS_INFO_STREAM("lin y: " << my_twist.twist.twist.linear.y);
+            ROS_INFO_STREAM("ang z: " << my_twist.twist.twist.angular.z);
             
-            ++my_twist[idx].header.seq;
+            mutex.unlock();
         }
     }
     prev_gray[idx] = frame_gray;
@@ -59,32 +98,37 @@ void optical_localization::image_callback1(const sensor_msgs::Image::ConstPtr& i
     process_image(image, 1);
 }
 
+void optical_localization::image_callback2(const sensor_msgs::Image::ConstPtr& image) {
+    process_image(image, 2);
+}
+
+void optical_localization::image_callback3(const sensor_msgs::Image::ConstPtr& image) {
+    process_image(image, 3);
+}
+
 optical_localization::optical_localization(int argc, char *argv[]) {
+    // TODO pixels_per_metre_traversed is different for each cam
+    // TODO camera axes tranforms
     std::fill(pixels_per_metre_traversed, pixels_per_metre_traversed + NUM_CAMS, -1503.1124320333);
     std::fill(rotation_velocity_scale, rotation_velocity_scale + NUM_CAMS, 1.0);
-    
-    for(unsigned int i = 0;i < NUM_CAMS;++i) {
-        translation_current[i] = (cv::Mat_<double>(2,1) << 0, 0);
-        scale_x[i] = 1.0;
-        scale_y[i] = 1.0;
-        rotation[i] = 0.0;
-        my_twist[i].twist.twist.linear.z = 0;
-        my_twist[i].twist.twist.angular.x = 0;
-        my_twist[i].twist.twist.angular.y = 0;
-        my_twist[i].header.seq = 0;
-        for(unsigned int j = 0;j < 36;++j) {
-            my_twist[i].twist.covariance[j] = 0;
-        }
-        my_twist[i].header.frame_id = "base_link";
-    }
+    std::fill(is_first, is_first + NUM_CAMS, true);
 
     ros::init(argc, argv, "optical_localization");
     ros::NodeHandle n("~");
     
+    my_twist.header.seq = 0;
+    my_twist.header.frame_id = "base_link";
+    
+    for(unsigned int i = 0;i < NUM_CAMS;++i) {
+        my_twist.twist.covariance[i] = 0;
+    }
+    
     sub[0] = n.subscribe("/optical_localization_cam0/image_raw", 1, &optical_localization::image_callback0, this);
-    pub[0] = n.advertise<geometry_msgs::TwistWithCovarianceStamped>("/owr/optical_localization_twist0", 10, true);
     sub[1] = n.subscribe("/optical_localization_cam1/image_raw", 1, &optical_localization::image_callback1, this);
-    pub[1] = n.advertise<geometry_msgs::TwistWithCovarianceStamped>("/owr/optical_localization_twist1", 10, true);
+    sub[2] = n.subscribe("/optical_localization_cam2/image_raw", 1, &optical_localization::image_callback2, this);
+    sub[3] = n.subscribe("/optical_localization_cam3/image_raw", 1, &optical_localization::image_callback3, this);
+    
+    pub = n.advertise<geometry_msgs::TwistWithCovarianceStamped>("/owr/optical_localization_twist", 10, true);
 }
 
 void optical_localization::run() {
