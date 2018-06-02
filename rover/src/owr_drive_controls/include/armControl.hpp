8feac77708df901ff8375f7dac91a1b4c12925ed
Elliott Smith <cbrakespear@hotmail.com.au>
2018-06-02 12:09:19 +1000

    Beginning work

---
+++ rover/src/owr_drive_controls/include/armControl.hpp
@@ -0,0 +1,8 @@
+/*
+ * Original Author: Elliott Smith
+ * Editors:
+ * Date Started: 2/6/18
+ * Purpose: Convert arm joystick message into arm joint control
+ *
+ */
+
---
+++ rover/src/owr_drive_controls/src/armControl.cpp
@@ -0,0 +1,9 @@
+/*
+ * Original Author: Elliott Smith
+ * Editors:
+ * Date Started: 2/6/18
+ * Purpose: Convert arm joystick message into arm joint control
+ *
+ */
+
+#include "armControl.hpp"
