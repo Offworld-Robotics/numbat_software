1708e516574c357580bfafbae230c5a491045af1
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-08-23 05:26:02 +1000

    RSC-2 #time 1h Publish dummy data; fails if there is a listener

--- rover/src/science_module/scripts/science_module_talker.py
+++ rover/src/science_module/scripts/science_module_talker.py
@@ -8,9 +8,16 @@ def talker():
     rospy.init_node('science_module_publisher', anonymous=True)
     rate = rospy.Rate(10) # Hz
     while not rospy.is_shutdown():
-        my_msg = 'Hello world %s' % (rospy.get_time())
-        rospy.loginfo(my_msg)
-        pub.publish(my_msg)
+        science_msg = science()
+        science_msg.temperature = 23
+        science_msg.humidity = 56
+        science_msg.mag_x = 12.67
+        science_msg.mag_y = 45,78
+        science_msg.mag_z = 133.89
+        science_msg.colour_temperature = 12
+        science_msg.illuminance = 89
+        rospy.loginfo(science_msg)
+        pub.publish(science_msg)
         rate.sleep()
 
 if __name__ == '__main__':
