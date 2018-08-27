910d0f6c73834c5f1c9f48b30a2182bfc48cfb72
hjed <harry@dayfamilyweb.com>
2018-08-27 22:39:13 +1000

    LR-23 #time 10m conversion code compiles, appears to be working

--- rover/src/owr_drive_controls/src/Cmd_Vel_Conversion.cpp
+++ rover/src/owr_drive_controls/src/Cmd_Vel_Conversion.cpp
@@ -9,13 +9,13 @@
  * ros_node: owr_cmd_vel_conversion
  */
 
-#include "Cmd_Vel_Conversion.cpp"
+#include "Cmd_Vel_Conversion.hpp"
 
 /**
  * Rover's turn radius in meters.
  * TODO: test this
  */
-static constexpr double TURN_RADIUS = 1.0;
+static const double TURN_RADIUS = 1.0;
 
 int main(int argc, char ** argv) {
     ros::init(argc, argv, "owr_cmd_vel_conversion");
@@ -26,8 +26,8 @@ int main(int argc, char ** argv) {
 
 Cmd_Vel_Conversion::Cmd_Vel_Conversion() {
     ros::TransportHints transportHints = ros::TransportHints().tcpNoDelay();
-    cmd_vel_sub = nh.subscribe<geometry_msgs::Twist>(TOPIC_IN, 1, &Cmd_Vel_Conversion::receive_vel_message, this, transportHints);
-    cmd_vel_pub = nh.advertise<geometry_msgs::Twist(TOPIC_OUT, 1, true);
+    cmd_vel_sub = nh.subscribe<geometry_msgs::Twist>(IN_TOPIC, 1, &Cmd_Vel_Conversion::receive_vel_msg, this, transportHints);
+    cmd_vel_pub = nh.advertise<geometry_msgs::Twist>(OUT_TOPIC, 1, true);
 }
 
 void Cmd_Vel_Conversion::receive_vel_msg(const geometry_msgs::Twist::ConstPtr & vel_msg) {
@@ -38,9 +38,17 @@ void Cmd_Vel_Conversion::receive_vel_msg(const geometry_msgs::Twist::ConstPtr & 
         return;
     }
 
-
+    geometry_msgs::Twist out_msg;
     // I think that we want the "tangential velocity" for our y
     // Vt(y) = gama*radius
-    vel_msg->linear.y = vel_msg->angular.z * TURN_RADIUS;
+    out_msg.linear.y = vel_msg->angular.z * TURN_RADIUS;
+    out_msg.linear.x = vel_msg->linear.x;
+    cmd_vel_pub.publish(out_msg);
 
+}
+
+void Cmd_Vel_Conversion::run() {
+    while(ros::ok()) {
+        ros::spin();
+    }
 }
