35ad266af0cea4053cc0a81d45d9ddf215a6275f
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-09-11 07:10:29 +0200

    Add license headers and comments

--- rover/src/science_module/scripts/science_module_listener.py
+++ rover/src/science_module/scripts/science_module_listener.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
+"""
+Processes data from science module topics
+@author: (Original Author) Sajid Ibne Anower
+@authors: (Editors)
+Purpose: Subscriber for science module
+@copyright: This code is released under the MIT [GPL for embeded] License.
+Copyright BLUEsat UNSW, 2018
+"""
+
 import rospy
 from owr_messages.msg import science
 
+# This is where the processing would happen.
+# Awaiting further specifications on exactly what should happen here
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + " I got this data %s", data)
 
--- rover/src/science_module/scripts/science_module_talker.py
+++ rover/src/science_module/scripts/science_module_talker.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+"""
+Publishes data to science module topics
+@author: (Original Author) Sajid Ibne Anower
+@authors: (Editors)
+Purpose: Publisher for science module
+@copyright: This code is released under the MIT [GPL for embeded] License.
+Copyright BLUEsat UNSW, 2018
+"""
+
 import rospy
 from owr_messages.msg import science
 
@@ -9,6 +18,8 @@ def talker():
     rate = rospy.Rate(10) # Hz
     while not rospy.is_shutdown():
         science_msg = science()
+        # When science module is ready, the following values would
+        # come from the sensors
         science_msg.temperature = 23
         science_msg.humidity = 56
         science_msg.mag_x = 12.67
@@ -16,6 +27,9 @@ def talker():
         science_msg.mag_z = 133.89
         science_msg.colour_temperature = 12
         science_msg.illuminance = 89
+        science_msg.weight = 4.56
+        science_msg.moisture = 17.99
+        # End of dummy info
         rospy.loginfo(science_msg)
         pub.publish(science_msg)
         rate.sleep()
