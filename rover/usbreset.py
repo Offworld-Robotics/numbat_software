34a929f964a3448d5dd9da6cb6b92d56f1d2adc9
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-09-12 14:57:39 +0200

    COnvert module to python 2

--- rover/usbreset.py
+++ rover/usbreset.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import os
 import fcntl
 import sys
 
 device = sys.argv[1]
-print("resetting driver:" + device)
+print "resetting driver:", device
 USBDEVFS_RESET= 21780
 
 try:
     with open(device ,'w', os.O_WRONLY) as f:
         fcntl.ioctl(f, USBDEVFS_RESET, 0)
-        print("Successfully reset usb")
-except Exception as msg:
-    print("Failed to reset device: " + msg)
+        print "Successfully reset usb"
+except Exception, msg:
+    print "Failed to reset device: ", msg
