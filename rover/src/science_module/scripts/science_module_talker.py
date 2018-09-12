0b4aa6943c0bb8f3de259f7568fe57d0eb6e7a13
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-09-13 00:23:02 +0200

    Publish int to science/request

--- rover/src/science_module/scripts/science_module_talker.py
+++ rover/src/science_module/scripts/science_module_talker.py
@@ -13,23 +13,12 @@ import rospy
 from owr_messages.msg import science
 
 def talker():
-    pub = rospy.Publisher('science/request', int, queue_size=10)
+    pub = rospy.Publisher('/science/request', int, queue_size=10)
     rospy.init_node('science_module_publisher', anonymous=True)
     rate = rospy.Rate(10) # Hz
     while not rospy.is_shutdown():
-        science_msg = science()
-        # When science module is ready, the following values would
-        # come from the sensors
-        science_msg.temperature = 23
-        science_msg.humidity = 56
-        science_msg.mag_x = 12.67
-        science_msg.mag_y = 45.78
-        science_msg.mag_z = 133.89
-        science_msg.colour_temperature = 12
-        science_msg.illuminance = 89
-        science_msg.weight = 4.56
-        science_msg.moisture = 17.99
-        # End of dummy info
+        # dummy
+        science_msg = 23
         rospy.loginfo(science_msg)
         pub.publish(science_msg)
         rate.sleep()
