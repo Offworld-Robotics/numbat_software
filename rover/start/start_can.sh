1003b2f2bd5a9d9141824dc7aa1a4c1353279993
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-09-13 05:16:37 +1000

    Add delay in can startup

--- rover/start/start_can.sh
+++ rover/start/start_can.sh
@@ -3,5 +3,6 @@
 source ~/ros_can_nodes/devel/setup.bash
 sudo slcand -o -c -s6 /dev/serial/by-id/usb-CANtact_CANtact_dev_00000000001A-if00 can0;
 sudo ifconfig can0 up
+sleep 1
 sudo ifconfig can0 txqueuelen 1000
 rosrun ros_can_nodes ros_can_nodes 
