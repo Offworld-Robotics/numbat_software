3cf3afcdb9a7dcf7dda1f509305afe58d90a4645
Shanil <shanildesilva@gmail.com>
2019-03-30 14:51:23 +1100

    fixed a couple small issues #time 2h

--- rover/src/owr_usb_reset/usb.py
+++ rover/src/owr_usb_reset/usb.py
@@ -8,7 +8,7 @@ from std_msgs.msg import String,Int16
 after %sudo	ALL=(ALL:ALL) ALL. This allows the usb reset program to run without needing a password for 
 sudo. Eneter password manually while running causes a problem 
 """
-global devices
+#global devices
 
 def idmap(device):
 	if (device[23:27] == "1d6b"):
@@ -17,16 +17,24 @@ def idmap(device):
 		return device[0:32]
 
 def callback(data):
-	print(data.data)
-	"""filepath = "/dev/bus/usb" + devices[data.data][0:32]
-	rospy.info("Usb reset")
-	command = "sudo ./usb_reset " + filepath
-	subprocess.call(command,shell=True)
-	rospy.loginfo("%s reset", data.data) """
+	subprocess.call("cd /dev/bus/usb",shell=True)
+	devices = subprocess.check_output(["lsusb"])
+	devices = devices.split("\n")
+	if(data.data < len(devices) - 1): 
+		devices[data.data].replace(' ', '')
+		print(devices[data.data])
+		filepath = "/dev/bus/usb/" + devices[data.data][4:7]
+		filepath = filepath + "/" + devices[data.data][15:18]
+		print(filepath)
+		command = "sudo ./usb_reset " + filepath
+		subprocess.call(command,shell=True)
+		rospy.loginfo("%s reset", data.data) 
+	else:
+		print("Out of range")
 
 def listener():
 	rospy.init_node('usb_reset', anonymous=True)
-	rospy.Subscriber("rover/usb/reset", String, callback)
+	rospy.Subscriber("rover/usb/reset", Int16, callback)
 
 def talker():
 	pub = rospy.Publisher('/rover/usb', String, queue_size=1)
@@ -40,7 +48,7 @@ def talker():
 		device_string = idmap(devices[0])
 		for i in range(1,len(devices)):
     			device_string = device_string + "#/#/" + idmap(devices[i])
-		rospy.loginfo(devices)
+		rospy.loginfo(device_string)
 		pub.publish(device_string)
 		rate.sleep()
 
