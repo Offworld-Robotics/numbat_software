40570d6c2b80721c10fa9d6c5cfd13d7d112bdec
ros <z5165065@ad.unsw.edu.au>
2019-08-03 15:17:22 +1000

    LR-32 #time 2h added finding position from 2 ar tags

--- rover/src/owr_positioning/include/artags.h
+++ rover/src/owr_positioning/include/artags.h
@@ -21,6 +21,11 @@
 #include <ar_track_alvar_msgs/AlvarMarkers.h>
 #include <ar_track_alvar_msgs/AlvarMarker.h>
 
+#include <math.h>
+#include <stdio.h>
+
+#define THRESHOLD 0.0001
+
 class artag_localization {
     private:
     //stuff to setup the node	
@@ -37,13 +42,22 @@ class artag_localization {
     nav_msgs::Odometry odomMsg;
 
     //transform messages
-    geometry_msgs::TransformStamped arTransform;    //tranform of ar tag in world frame (for the actual location of the tag)
-    geometry_msgs::TransformStamped relativeTransform; //tranform of ar tag relative to rover (for the location of the tag from what the rover sees)
+    //geometry_msgs::TransformStamped arTransform;    //tranform of ar tag in world frame (for the actual location of the tag)
+    //geometry_msgs::TransformStamped relativeTransform; //tranform of ar tag relative to rover (for the location of the tag from what the rover sees)
     //geometry_msgs::TransformStamped roverTransform; dont think i need this one anymore
 
     //callback function
     void callback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg);
-	
+    
+    //functions to get transforms
+    geometry_msgs::TransformStamped getMarkerLocation(int id);
+    geometry_msgs::TransformStamped getMarkerDistance(int id);
+
+    //function for calculating the location of the rover from 2 ar tags
+    geometry_msgs::Pose getPosition(double x1, double y1, double r1, double x2, double y2, double r2, double gx, double gy);
+    
+    geometry_msgs::Pose locateRover();
+    
     public:
     //functions for initialising and running the node
     artag_localization();
--- rover/src/owr_positioning/src/artags.cpp
+++ rover/src/owr_positioning/src/artags.cpp
@@ -72,6 +72,7 @@ artag_localization::artag_localization() : tfBuffer(), tfListener(tfBuffer)  {
     odomMsg.child_frame_id = "base_link";
     odomMsg.header.seq = 0;
     //setup the ar transform, this is the markers position in world frame
+    /*
     arTransform.header.frame_id = "";
     arTransform.child_frame_id = "location";
     arTransform.header.seq = 0;
@@ -85,6 +86,7 @@ artag_localization::artag_localization() : tfBuffer(), tfListener(tfBuffer)  {
     //localTransform.header.frame_id = "camera_link";
     //localTransform.child_frame_id = "ar_tag";
     //localTransform.header.seq = 0;
+    */
 }
 
 
@@ -98,79 +100,164 @@ void artag_localization::callback(const ar_track_alvar_msgs::AlvarMarkers::Const
     //find the amount of tags found
     int size = msg->markers.size();
 
-    //for now we will only look at the first tag
+    //Dont do anything if no tags were found 
     if (size != 0){
-	ROS_INFO("tag found!");
-	//get the id of the first tag
-	int id = msg->markers[0].id;
-	
-    //get header of ar tag message to be header of odom message
-    ++odomMsg.header.seq;
-    odomMsg.header.stamp = ros::Time::now();
-    odomMsg.header.frame_id = "odom";
+      ROS_INFO("tag found!");
+      //get the id of the first tag
+      int id = msg->markers[0].id;
+	  
+      //get header of ar tag message to be header of odom message
+      ++odomMsg.header.seq;
+      odomMsg.header.stamp = ros::Time::now();
+      odomMsg.header.frame_id = "odom";
+      
+      //if only 1 marker was found then use a probability appraoch
+      if(size == 1){
+	  //get the trransform of the marker from the rover
+	  geometry_msgs::TransformStamped distanceTf = getMarkerDistance(id);
+	  
+	  //get the trasnform for the markers real location
+	  geometry_msgs::TransformStamped markerTf = getMarkerLocation(id);
+      }
+      
+      //if 2 tags are seen then calculate the position of the rover, only usse first 2 tags found
+      if (size > 1){
+		  //find location of first tag
+		  geometry_msgs::TransformStamped markerTf1 = getMarkerLocation(id);
+		  geometry_msgs::TransformStamped distanceTf1 = getMarkerDistance(id);
+		  
+		  //find location of second tag
+		  id = msg->markers[1].id;
+		  geometry_msgs::TransformStamped markerTf2 = getMarkerLocation(id);
+		  geometry_msgs::TransformStamped distanceTf2 = getMarkerDistance(id);
+          
+          //get the position
+          double marker1x = markerTf1.transform.translation.x;
+          double marker1y = markerTf1.transform.translation.y;
+          double marker2x = markerTf2.transform.translation.x;
+          double marker2y = markerTf2.transform.translation.y;
     
-    //get the name of the markers frame, this is ar_marker_(id) and save this as a string
-    //to do this we use stringstream to convert the id to a string and attach it to the end
-    //of the string "ar_marker_"
+          //get the current guess of the rovers location
+          geometry_msgs::TransformStamped roverTf;
+/*
+	        try {
+	            roverTf = tfBuffer.lookupTransform("odom", "base_link", ros::Time(0));
+                } catch (tf2::TransformException & ex) {
+                    ROS_WARN("%s", ex.what());
+                    ros::Duration(1.0).sleep();
+                }
+
+          double guessX = roverTf.transform.translation.x;
+          double guessY = roverTf.transform.translation.y;
+*/
+          double guessX = 0;
+          double guessY = 0;
+          //get the distance of the tags from the rover
+          double distance1 = sqrt(pow(distanceTf1.transform.translation.x, 2) + pow(distanceTf1.transform.translation.y, 2));
+          double distance2 = sqrt(pow(distanceTf2.transform.translation.x, 2) + pow(distanceTf2.transform.translation.y, 2));
+          
+          //calculate the postion of the rover
+          geometry_msgs::Pose roverPose;          
+          roverPose = getPosition(marker1x, marker1y, distance1, marker2x, marker2y, distance2, guessX, guessY);
+	      ROS_INFO("****************************************************");
+      }
+    } else {
+    //if no tags were found
+	ROS_INFO("No tags found! :(");
+    }   
+}
+
+geometry_msgs::TransformStamped artag_localization::getMarkerLocation(int id){
+    //get the name of the marker
     std::stringstream ss;
     ss << id;
-	std::string markerFrame = "ar_marker_";
-	markerFrame += ss.str();
-
-	//get the transform of the camera to the tag	
-    try {
-	    relativeTransform = tfBuffer.lookupTransform(markerFrame, "camera_link", ros::Time(0));
-        } catch (tf2::TransformException & ex) {
-            ROS_WARN("%s", ex.what());
-            ros::Duration(1.0).sleep();
-        }
+    std::string markerFrame = "marker";
+    markerFrame += ss.str();
     
-    //the local transform uses axis relative to the camera, apply a rotation transformation
-    //so that it matches the rovers axis
-
-	//look up transform of tag in the world frame
-	std::string worldMarkerFrame = "marker";
-	worldMarkerFrame += ss.str();
+   
+    geometry_msgs::TransformStamped tf;
+    
+    //get the transform of the camera to the tag	
 	try {
-	    arTransform = tfBuffer.lookupTransform("odom", worldMarkerFrame, ros::Time(0));
+	    tf = tfBuffer.lookupTransform("odom", markerFrame, ros::Time(0));
+        } catch (tf2::TransformException & ex) {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+        }
+        
+    return tf;
+}
+
+geometry_msgs::TransformStamped artag_localization::getMarkerDistance(int id){
+    //get the name of the marker
+    std::stringstream ss;
+    ss << id;
+    std::string markerFrame = "ar_marker_";
+    markerFrame += ss.str();
+    
+    geometry_msgs::TransformStamped tf;
+    
+    //get the transform of the camera to the tag	
+    try {
+	tf = tfBuffer.lookupTransform(markerFrame, "camera_link", ros::Time(0));
         } catch (tf2::TransformException & ex) {
             ROS_WARN("%s", ex.what());
             ros::Duration(1.0).sleep();
         }
     
+    return tf;
+}
 
-    //convert to odom msg, subtract the relative measurement vector from the actual ar tag location
-    odomMsg.pose.pose.position.x = arTransform.transform.translation.x - relativeTransform.transform.translation.x;
-    odomMsg.pose.pose.position.y = arTransform.transform.translation.y - relativeTransform.transform.translation.y;
-    odomMsg.pose.pose.position.z = arTransform.transform.translation.z - relativeTransform.transform.translation.z;
 
-	tf2::Quaternion quat_ar;
-	tf2::Quaternion quat_local;
-	tf2::convert(arTransform.transform.rotation, quat_ar);
-	tf2::convert(relativeTransform.transform.rotation, quat_local);
-    //negate the w component so that when multiplying the 2nd quat is inversed
-	quat_local[3] = -quat_local[3]; 
-    
-    //multiply quaternions to get the quaternion for camera in the world frame
-	odomMsg.pose.pose.orientation = tf2::toMsg(quat_ar * quat_local);
+geometry_msgs::Pose artag_localization::getPosition(double x1, double y1, double r1, double x2, double y2, double r2, double gx, double gy)
+{
+    geometry_msgs::Pose pose;
+    // find the two probable points using a circle...
+    // distance between centers...
+    double d = sqrt(abs((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
 
-    //convert transform message from geomertry_msg to tf
-    tf2::Transform tf;
-    geometry_msgs::Transform geoTF;
-    tf2::Stamped<tf2::Transform> relativetf;
-    tf2::fromMsg(relativeTransform, relativetf);
-    tf2::Stamped<tf2::Transform> artf;
-    tf2::fromMsg(arTransform, artf);
-    tf = artf.inverseTimes(relativetf);
-    geoTF = tf2::toMsg(tf);
-    //arTransform = tf2::toMsg(localtf);
-    arTransform.transform = geoTF;
-    arTransform = tf2::toMsg(artf);
-    arTransform.header.frame_id = "camera_link";
-    arTransform.child_frame_id = "location";
-    //subtract the observed distance to the ar tag from the real world location
+    // check if two points exist
+    if (d > r1 + r2 || d < abs(r1 - r2)) {
+        // circles are separate (or contained)
+        //printf("No intersection points!!\n");
+    }
+    else if (d < THRESHOLD) {
+        // coincident!
+        //printf("Coincident!");
+    }
 
-    /*
+    double centerDist = (r1 * r1 - r2 * r2 + d * d) / (2 * d);
+    double height = sqrt(abs(r1 * r1 - centerDist * centerDist));
+
+    // Evaluate center point
+    double cx = x1 + centerDist * (x2 - x1) / d;
+    double cy = y1 + centerDist * (y2 - y1) / d;
+
+    // Evaluate postion
+    double px1 = cx + height * (y2 - y1) / d;
+    double px2 = cx - height * (y2 - y1) / d;
+    double py1 = cy - height * (x2 - x1) / d;
+    double py2 = cy + height * (x2 - x1) / d;
+
+    // Find the point closest to the guess.
+    double d1 = sqrt(abs(((px1 - gx) * (px1 - gx)) + ((py1 - gy) * (py1 - gy))));
+    double d2 = sqrt(abs(((px2 - gx) * (px2 - gx)) + ((py2 - gy) * (py2 - gy))));
+
+    if (d1 > d2) {
+        //printf("(x, y) = (%lf, %lf)\n", px2, py2);
+        pose.position.x = px2;
+        pose.position.y = py2;
+    }
+    else {
+        //printf("(x, y) = (%lf, %lf)\n", px1, py1);
+        pose.position.x = px1;
+        pose.position.y = py1;
+    }
+    //printf("%lf and 2: %lf\n", d1, d2);
+    return pose;
+}
+/* random code i used before thats useless but im keeping in case i need it again
+ *     /*
     //get the transofrm of the camera relative to the rover
 	try {
 	    roverTransform = tfBuffer.lookupTransform("rover_model", "camera_frame", ros::Time(0));
@@ -217,10 +304,39 @@ void artag_localization::callback(const ar_track_alvar_msgs::AlvarMarkers::Const
     //publish camera transform in world frame
 	tfBroadcaster.sendTransform(roverTransform);
 	*/
-    pub.publish(odomMsg);
-    } else {
-    //if no tags were found
-	ROS_INFO("No tags found! :(");
-    }   
-}
+    
+        /*
+    //convert to odom msg, subtract the relative measurement vector from the actual ar tag location
+    odomMsg.pose.pose.position.x = arTransform.transform.translation.x - relativeTransform.transform.translation.x;
+    odomMsg.pose.pose.position.y = arTransform.transform.translation.y - relativeTransform.transform.translation.y;
+    odomMsg.pose.pose.position.z = arTransform.transform.translation.z - relativeTransform.transform.translation.z;
 
+	tf2::Quaternion quat_ar;
+	tf2::Quaternion quat_local;
+	tf2::convert(arTransform.transform.rotation, quat_ar);
+	tf2::convert(relativeTransform.transform.rotation, quat_local);
+    //negate the w component so that when multiplying the 2nd quat is inversed
+	quat_local[3] = -quat_local[3]; 
+    
+    //multiply quaternions to get the quaternion for camera in the world frame
+	odomMsg.pose.pose.orientation = tf2::toMsg(quat_ar * quat_local);
+
+    //convert transform message from geomertry_msg to tf
+    tf2::Transform tf;
+    geometry_msgs::Transform geoTF;
+    tf2::Stamped<tf2::Transform> relativetf;
+    tf2::fromMsg(relativeTransform, relativetf);
+    tf2::Stamped<tf2::Transform> artf;
+    tf2::fromMsg(arTransform, artf);
+    tf = artf.inverseTimes(relativetf);
+    geoTF = tf2::toMsg(tf);
+    //arTransform = tf2::toMsg(localtf);
+    arTransform.transform = geoTF;
+    arTransform = tf2::toMsg(artf);
+    arTransform.header.frame_id = "camera_link";
+    arTransform.child_frame_id = "location";
+    //subtract the observed distance to the ar tag from the real world location
+
+
+    pub.publish(odomMsg);
+    */
